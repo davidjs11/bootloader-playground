fbf09cb8605f6812783e21f73a2a5e655f6b5545
davidjs11 <davidjandulasanchez11@gmail.com>
2023-08-26 03:03:45 +0200

    testing functions

--- 04-first-kernel/kernel.c
+++ 04-first-kernel/kernel.c
@@ -1,15 +1,16 @@
 /// kernel.c ////////////////////////////////
 
-// function test
-void function(char *ptr, char val)
-{
-    *ptr = val;
+// print a string into the vga text buffer
+void print_vga(char *buffer, const char *val) {
+    // print until last character
+    while(*val != 0) {
+        *buffer = *val;
+        buffer += 2;
+        val++;
+    }
 }
 
-// main program
-void main()
-{
-    // Display '!' as the first character in the screen
+void main() {
     char *videoMemory = (char *) 0xB8000;
-    function(videoMemory, '!');
+    print_vga(videoMemory, "kernel.c loaded successfully");
 }
--- 04-first-kernel/kernel_entry.asm
+++ 04-first-kernel/kernel_entry.asm
@@ -1,4 +1,8 @@
-[bits 32]
-[extern main]
-call main
+; --- kernel-entry.asm ----------------------
+; initializes the main function of the kernel, instead of
+; running the first piece of code of the program.
+
+[bits 32]       ; set 32-bit mode
+[extern main]   ; use main declared in kernel.c
+call main        
 jmp $
